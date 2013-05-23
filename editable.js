c9c8ec70cf1b35fab2fefee5093eeb01d631c2cc
lexoyo <lexoyo@lexa-debian.lexa.org>
2013-05-23 23:39:41 +0200

    one mode only for edit in place and dom

--- editable.js
+++ editable.js
@@ -1,55 +1,85 @@
 (function( $, undefined ) {
 
-$.widget("silexlabs.editable", {
-	version: "1.0.0",
+$.widget('silexlabs.editable', {
+	version: '1.0.0',
 	options: {
-        mode: "in-place",
-        element: ".editable-element",
+        isContainer: false,
 	},
 	// _setOption is called for each individual option that is changing
 	_setOption: function( key, value ) {
-		console.log("set option "+key+"="+value);
+		console.log('set option '+key+'='+value);
+		this.options[key] = value;
 		switch(key){
-			case "mode":
-				this.setMode(value);
+			case 'isContainer':
+				this.disableEditable();
+				this.enableEditable();
 				break;
-			case "disabled":
+			case 'disabled':
 				if (value==true){
-					this.setMode("disabled");					
+					this.disableEditable();
 				}
 				else{
-					this.setMode(this.options.mode);
+					this.enableEditable();
 				}
 				break;
 		}
 	},
 	_create: function() {
-		this.setMode("created");
+		console.log("_create ");
+		this.enableEditable();
 	},
 	_destroy: function() {
-		this.setMode("destroyed");
+		console.log("_destroy ");
+		this.element.resizable('destroy').draggable('destroy');
+		if (this.options.isContainer){
+			this.element.droppable('destroy');
+		}
 	},
-	setMode : function (mode) {
-		console.log("setMode "+mode);
-		switch (mode){
-			case "created":
-				$( this.options.element ).resizable().draggable({ 
-					containment: "parent"
-				});
-				break;
-			case "destroyed":
-				$( this.options.element ).resizable('destroy').draggable('destroy');
-				break;
-			case "disabled":
-				$( this.options.element ).resizable('disable').draggable('disable');
-				break;
-			case "in-place":
-				$( this.options.element ).resizable('enable').draggable('enable');
-				break;
-			case "dom":
-				$( this.options.element ).resizable('disable').draggable('disable');
-				console.error("not implemented");
-				break;
+	disableEditable: function(){
+		console.log("disable ");
+		this.element.draggable({revert:undefined});
+		if (this.options.isContainer){
+			this.element.droppable('disable');
+		}
+		this.element.resizable('disable').draggable('disable');
+	},
+	enableEditable: function(){
+		console.log("enable ");
+		this.element.resizable();
+		this.element.draggable({ revert: 'invalid' });
+		this.element.resizable('enable').draggable('enable');
+		if (this.options.isContainer){
+			this.element.droppable({
+				// prevent propagation
+				greedy: true,
+				
+				drop: function( event, ui ) {
+					// reference to the elements
+					var dropped = ui.draggable;
+					var droppedFrom = $(dropped).parent();
+					var droppedTo = this;
+
+					// compute new position in the container
+					console.log('drop ');
+
+					// keep initial position
+					var initialOffset = $(dropped).offset();
+					
+					// move to the new container
+					$(dropped).detach().appendTo($(droppedTo));
+					
+					// compute new position
+					var newOffset = $(dropped).offset();
+					var deltaTop = initialOffset.top - newOffset.top;
+					var deltaLeft = initialOffset.left - newOffset.left;
+					var newPosTop = $(dropped).position().top + deltaTop;
+					var newPosLeft = $(dropped).position().left + deltaLeft;
+
+					// put back at the same position
+					$(dropped).css({top: newPosTop+'px',left: newPosLeft+'px'});
+		    	}
+		    });
+		    this.element.droppable('enable');
 		}
 	},
 });
